bc4c50a1d1385f9e73b0b465f7eacdca71bd3fa9
Wirys505 <90320812+Wirys505@users.noreply.github.com>
2021-10-06 10:02:16 +0300

    Add files via upload

--- Labs/Lab5/GUI.java
+++ Labs/Lab5/GUI.java
@@ -2,5 +2,31 @@ package Labs.Lab5;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class GUI {
+public class GUI extends JFrame{
+    JPanel[] pnl = new JPanel[20];
+    public GUI() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(4, 5));
+        for (int i = 0; i < pnl.length; i++) {
+            int r = (int) (Math.random() * 255);
+            int b = (int) (Math.random() * 255);
+            int g = (int) (Math.random() * 255);
+            pnl[i] = new GUI.MyRectangleJPanel();
+            pnl[i].setBackground(new Color(r, g, b));
+            add(pnl[i]);
+        }
+        setSize(800, 500);
+    }
+    class MyRectangleJPanel extends JPanel {
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            int n1 = (int) (Math.random() * 140);
+            int n2 = (int) (Math.random() * 95);
+            g.setColor(Color.CYAN);
+            g.fillRect(n1, n2, 20, 30);
+        }
+    }
+    public static void main(String[] args) {
+        new GUI().setVisible(true);
+    }
 }
